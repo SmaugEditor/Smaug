3522a65bc81f52fe42c1c49a48be4fb7a003c770
ozxybox <kieferstashu@gmail.com>
2020-08-17 11:02:12 -0500

    Spin the object editor
    lol

--- Smaug/selectedview.cpp
+++ Smaug/selectedview.cpp
@@ -12,11 +12,15 @@ void CSelectedView::Init(bgfx::ViewId viewId, int width, int height, uint32_t cl
 	CBaseView::Init(viewId, width, height, clearColor);
 }
 
+static float time = 0;
+
 void CSelectedView::Draw(float dt)
 {
+	time += dt;
 	CBaseView::Draw(dt);
 
-	glm::mat4 view = glm::lookAt(glm::vec3(10.0f, 85.0f, 40.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 view = glm::lookAt(glm::vec3(0, 85.0f, 85.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	view = glm::rotate(view, time, glm::vec3(0, 1, 0));
 	glm::mat4 proj = glm::perspective(glm::radians(60.0f), float(m_width) / m_height, 0.1f, 800.0f);
 	bgfx::setViewTransform(m_viewId, &view[0][0], &proj[0][0]);
 
