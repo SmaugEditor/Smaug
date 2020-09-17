7731c27d726484dd4880f4f266d4408b48e40dbe
ozxybox <kieferstashu@gmail.com>
2020-09-16 22:29:40 -0500

    Add toolbox selection coloring

--- Smaug/toolbox.cpp
+++ Smaug/toolbox.cpp
@@ -8,10 +8,18 @@ void CToolBox::ShowToolBox()
 
 void CToolBox::Update()
 {
+	// UI
 	if (ImGui::Begin("Tool Box"))
 	{
+		bool colorChange = false;
 		for (int i = 0; i < m_tools.size(); i++)
 		{
+			// If this tool is selected, it gets to have a fancy new color!
+			if (m_tools[i] == m_currentTool)
+			{
+				ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(255, 0, 0, 255));
+				colorChange = true;
+			}
 			if (ImGui::Button(m_tools[i]->GetName()))
 			{
 				if (m_currentTool)
@@ -25,6 +33,12 @@ void CToolBox::Update()
 					m_currentTool->Enable();
 				}
 			}
+			if (colorChange)
+			{
+				ImGui::PopStyleColor();
+				colorChange = false;
+			}
+
 		}
 		ImGui::End();
 	}
