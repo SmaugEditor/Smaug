816607bfdc9e980e7fd46f4d09714e6ee63bd85a
ozxybox <kieferstashu@gmail.com>
2020-09-25 14:37:10 -0500

    Add plane drawing to basicdraw

--- Smaug/basicdraw.cpp
+++ Smaug/basicdraw.cpp
@@ -12,7 +12,7 @@
  * License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
  */
 
-struct PosColorVertex
+struct CubePosColorVertex
 {
 	float x;
 	float y;
@@ -28,9 +28,9 @@ struct PosColorVertex
 	}
 	static bgfx::VertexLayout ms_layout;
 };
-bgfx::VertexLayout PosColorVertex::ms_layout;
+bgfx::VertexLayout CubePosColorVertex::ms_layout;
 
-static PosColorVertex s_cubeVertices[] =
+static CubePosColorVertex s_cubeVertices[] =
 {
 	{-1.0f,  1.0f,  1.0f, 0xff000000 },
 	{ 1.0f,  1.0f,  1.0f, 0xff0000ff },
@@ -44,14 +44,49 @@ static PosColorVertex s_cubeVertices[] =
 static const uint16_t s_cubeTriList[] = { 2, 1, 0, 2, 3, 1, 5, 6, 4, 7, 6, 5, 4, 2, 0, 6, 2, 4, 3, 5, 1, 3, 7, 5, 1, 4, 0, 1, 5, 4, 6, 3, 2, 7, 3, 6 };
 
 
+struct TexturedPlanePosColorVertex
+{
+	float x;
+	float y;
+	float z;
+	float u;
+	float v;
+
+	static void init()
+	{
+		ms_layout
+			.begin()
+			.add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+			.add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Float)
+			.end();
+	}
+	static bgfx::VertexLayout ms_layout;
+};
+bgfx::VertexLayout TexturedPlanePosColorVertex::ms_layout;
+
+static TexturedPlanePosColorVertex s_texturedPlaneVertices[] =
+{
+	{-1.0f,  0.0f, -1.0f, 0.0f, 0.0f },
+	{ 1.0f,  0.0f, -1.0f, 0.0f, 1.0f },
+	{ 1.0f,  0.0f,  1.0f, 1.0f, 1.0f },
+	{-1.0f,  0.0f,  1.0f, 1.0f, 0.0f },
+};
+static const uint16_t s_texturedPlaneTriList[] = { 0, 1, 2, 2, 3, 0 };
+
 
 CBasicDraw::CBasicDraw()
 {
-	PosColorVertex::init();
+	CubePosColorVertex::init();
+	TexturedPlanePosColorVertex::init();
 
-	m_cubeVertexBuf = bgfx::createVertexBuffer(bgfx::makeRef(s_cubeVertices, sizeof(s_cubeVertices)), PosColorVertex::ms_layout);
+	m_cubeVertexBuf = bgfx::createVertexBuffer(bgfx::makeRef(s_cubeVertices, sizeof(s_cubeVertices)), CubePosColorVertex::ms_layout);
 	m_cubeIndexBuf = bgfx::createIndexBuffer(bgfx::makeRef(s_cubeTriList, sizeof(s_cubeTriList)));
+
+	m_planeVertexBuf = bgfx::createVertexBuffer(bgfx::makeRef(s_texturedPlaneVertices, sizeof(s_texturedPlaneVertices)), TexturedPlanePosColorVertex::ms_layout);
+	m_planeIndexBuf = bgfx::createIndexBuffer(bgfx::makeRef(s_texturedPlaneTriList, sizeof(s_texturedPlaneTriList)));
+	
 }
+
 
 void CBasicDraw::Cube(glm::mat4 mtx)
 {
@@ -61,9 +96,16 @@ void CBasicDraw::Cube(glm::mat4 mtx)
 	//bgfx::setState(BGFX_STATE_DEFAULT);
 }
 
+void CBasicDraw::Plane(glm::mat4 mtx)
+{
+	bgfx::setTransform(&mtx[0][0]);
+	bgfx::setVertexBuffer(0, m_planeVertexBuf);
+	bgfx::setIndexBuffer(m_planeIndexBuf);
+}
 
 CBasicDraw& BasicDraw()
 {
 	static CBasicDraw s_basicDraw;
 	return s_basicDraw;
 }
+
--- Smaug/basicdraw.h
+++ Smaug/basicdraw.h
@@ -9,10 +9,14 @@ public:
 	CBasicDraw();
 
 	void Cube(glm::mat4 mtx);
+	void Plane(glm::mat4 mtx);
 private:
 
 	bgfx::VertexBufferHandle m_cubeVertexBuf;
 	bgfx::IndexBufferHandle m_cubeIndexBuf;
+
+	bgfx::VertexBufferHandle m_planeVertexBuf;
+	bgfx::IndexBufferHandle m_planeIndexBuf;
 };
 
 
