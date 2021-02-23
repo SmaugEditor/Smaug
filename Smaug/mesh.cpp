d61f560151c83c587ca5307257dbba9f3f78a27e
ozxybox <kieferstashu@gmail.com>
2021-02-22 22:35:08 -0600

    Add the start of shape cutting support

---
+++ Smaug/mesh.cpp
@@ -0,0 +1,215 @@
+#include "mesh.h"
+
+/*
+Process that happens here
+
+INPUT
+________________________
+|                      |
+|       ________       |
+|       |XXXXXX|       |
+|       |XXXXXX|       |
+|                      |
+|______________________|
+
+SLICE
+________________________
+|              |       |
+|______________|       |
+|       |XXXXXX|       |
+|       |XXXXXX|_______|
+|       |              |
+|_______|______________|
+
+TRIANGULATE
+
+________________________
+|\............ |\      |
+|_____________\|  \    |
+|\      |XXXXXX|    \  |
+|  \    |XXXXXX|______\|
+|    \  |\............ |
+|______\|_____________\|
+
+
+*/
+
+template<typename T>
+void swap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+// For use where we know which side's going to end up larger
+// Returns the new face
+face_t* sliceShapeFaceUnsafe(cutttableShape_t& shape, face_t* face, vertex_t* start, vertex_t* end)
+{
+	face_t* newFace = new face_t;
+	newFace->meshPart = &shape;
+	shape.faces.push_back(newFace);
+
+	halfEdge_t* heStart = start->edge;
+	halfEdge_t* heEnd   = end->edge;
+
+	
+	// Let's just let it keep end...
+	// We're totally stealing start though!
+	vertex_t* newEnd = new vertex_t{ end->vert, end->edge };
+
+	newFace->verts.push_back(newEnd);
+	newFace->sideCount = 1;
+
+	// We need to steal from our old and add to our new
+	for (halfEdge_t* he = heEnd; he != heStart; he = he->next)
+	{
+		newFace->verts.push_back(he->vert);
+		newFace->edges.push_back(he);
+		newFace->sideCount++;
+	}
+
+	// Set up our new HE from start to end on the small side
+	halfEdge_t* newHe = new halfEdge_t{ newEnd, nullptr, newFace, heEnd };
+	halfEdge_t* prevStart = newFace->edges[newFace->edges.size()-1];
+	prevStart->next = newHe;
+	prevStart->vert->edge = newHe;
+	newFace->edges.push_back(newHe);
+
+
+	// Abysmal
+	face->edges.clear();
+
+	vertex_t* newStart = new vertex_t{ start->vert, start->edge };
+	face->verts.push_back(newStart);
+	for (halfEdge_t* he = heStart; he != heEnd; he = he->next)
+	{
+		face->verts.push_back(he->vert);
+		face->edges.push_back(he);
+	}
+
+	// New HE for our larger half
+	halfEdge_t* newHeLarger = new halfEdge_t{ newStart, newHe, face, heEnd };
+	newHe->pair = newHeLarger;
+	halfEdge_t* prevEnd = newFace->edges[newFace->edges.size() - 1];
+	prevEnd->next = newHeLarger;
+	prevEnd->vert->edge = newHeLarger;
+	face->edges.push_back(newHeLarger);
+
+
+	return newFace;
+}
+
+// This will subdivide a shape's face from start to end
+// It will return the new face
+// This cut will only occur on ONE face
+face_t* sliceShapeFace(cutttableShape_t& shape, face_t* face, vertex_t* start, vertex_t* end)
+{
+	// No slice
+	if (start == end || !start || !end || !face)
+		return face;
+
+
+	// Don't like this much...
+	int between = 0;
+	for(vertex_t* vert = start; vert != end; vert = vert->edge->vert)
+	{
+		between++;
+	}
+
+	// Start should always be the clockwise start of the smaller part
+	if (between < face->edges.size() / 2.0f)
+	{
+		swap(start,    end);
+	}
+
+	// Now that we know which side's larger, we can safely slice
+	return sliceShapeFaceUnsafe(shape, face, start, end);
+}
+
+void triangluateShapeFaces(cutttableShape_t& shape)
+{
+	// As we'll be walking this, we wont want to walk over our newly created faces
+	// Store our len so we only get to the end of the predefined faces
+	size_t len = shape.faces.size();
+	for (size_t i = 0; i < len; i++)
+	{
+		face_t* face = shape.faces[i];
+		if (!face)
+			continue;
+		
+		// Discard Tris
+		if (face->edges.size() < 4)
+			continue;
+
+		
+		// In these loop, we'll progressively push the original face to become smaller and smaller
+		while(face->verts.size() > 3)
+		{
+			// Vert 0 will become our anchor for all new faces to connect to
+			vertex_t* v0 = face->verts[0];
+
+			vertex_t* end = face->verts[face->verts.size() - 2];
+
+			// Wouldn't it just be better to implement a quick version for triangulate? We're doing a lot of slices? Maybe just a bulk slicer?
+			sliceShapeFaceUnsafe(shape, face, v0, end);
+
+		};
+
+		
+	}
+
+}
+
+void defineShape(cutttableShape_t& shape)
+{
+	// Clear out our old faces
+	for (auto v : shape.faces)
+		if(v) delete v;
+	shape.faces.clear();
+
+	face_t* f = new face_t;
+	shape.faces.push_back(f);
+
+	// Let the face know our data
+	f->sideCount = shape.verts.size();
+	f->meshPart = &shape;
+
+	// Populate our face with HEs
+	vertex_t* lastVert = nullptr;
+	halfEdge_t* lastHe = nullptr;
+	for (glm::vec3& vert : shape.verts)
+	{
+		halfEdge_t* he = new halfEdge_t{};
+		he->face = f;
+		vertex_t* v = new vertex_t{ &vert };
+
+		// Should never be a situation where these both arent null or something
+		// If one is null, something's extremely wrong
+		if (lastHe && lastVert)
+		{
+			lastVert->edge = he;
+			lastHe->next = he;
+			lastHe->vert = v;
+		}
+
+		lastHe = he;
+		lastVert = v;
+		f->edges.push_back(he);
+		f->verts.push_back(v);
+	}
+
+	// Link up our last HE
+	if (lastHe && lastVert)
+	{
+		lastHe->next = f->edges.front();
+		lastHe->vert = f->verts.front();
+	}
+
+	//triangluateShapeFaces(shape);
+}
+
+void slicePlane(face_t* f, face_t** )
+{
+
+}
--- Smaug/mesh.h
+++ Smaug/mesh.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "glm/vec3.hpp"
-
+#include <vector>
 
 /*
 
@@ -24,41 +24,106 @@ P=vertex.edge.next.next.pair.next.vertex
 
 struct halfEdge_t;
 struct face_t;
+struct mesh_t;
 
-// Inherit from glm::vec3 for easy compatibility
-struct vertex_t : public glm::vec3 
+struct vertex_t
 {
+	// Should refer back to the face's verts
+	glm::vec3* vert;
+
 	// The edge that stems out of this vert
-	halfEdge_t* edge;
+	halfEdge_t* edge = nullptr;
 };
 
 
 struct halfEdge_t
 {
-	// This edge stems out of a vert. vert is the vert at the end of it.
-	vertex_t* vert;
+	// This edge stems out of a vertex. vert is the vertex at the end of this edge.
+	vertex_t* vert = nullptr;
 
 	// This edge is half of another edge. This is the other side of the edge located on another face.
-	halfEdge_t* pair;
+	halfEdge_t* pair = nullptr;
 
 	// The face this edge belongs to.
-	face_t* face;
+	face_t* face = nullptr;
 
 	// Next edge. Same as vert->edge.
-	halfEdge_t* next;
+	halfEdge_t* next = nullptr;
 };
 
 
-// Each face is a triangle
+struct meshPart_t;
 struct face_t
 {
-	// These edges use the verts 
-	halfEdge_t edges[3];
+	size_t sideCount;
 
-	vertex_t verts[3];
+	// These edges use the verts 
+	std::vector<halfEdge_t*> edges;
+
+	std::vector<vertex_t*> verts;
+
+	// What part of the mesh we belong to.
+	meshPart_t* meshPart;
+};
+
+// Axis Aligned Bounding Box. Absolute minimum and maximum of a shape
+struct aabb_t
+{
+	glm::vec3 min;
+	glm::vec3 max;
+};
+
+struct meshPart_t
+{
+	std::vector<face_t*> faces;
+	
+	// Calculated from face verts
+	//aabb_t aabb;
 };
 
 struct mesh_t
 {
+	std::vector<meshPart_t*> parts;
 
+	// Calculated from part aabbs
+	aabb_t aabb;
+};
+
+
+
+// One shape contains many HE'd faces
+// Used for large editing and faces for cutting
+//
+//  Shape
+//
+//      /\
+//     /  \
+//    /    \
+//   /      \
+//  /        \
+// /__________\
+//
+//  Faces of the shape after cut
+//
+//      /\
+//     /  \
+//    /____\
+//   / |XX| \
+//  /  |XX|  \
+// /___|__|___\
+
+struct cutttableShape_t : public meshPart_t
+{
+	// These define the shape 
+	// Must be defined in clockwise-faceout-order
+	// Editing this MUST be followed by redefinition!
+	std::vector<glm::vec3> verts;
+
+	// Inherited from meshPart_t
+	// These are triangluated faces within the shape
+	// Never depend on them post a refresh
+	//std::vector<face_t*> faces;
+
+	// These cut into this shape and form what the faces look like
+	std::vector<cutttableShape_t*> cutters;
 };
