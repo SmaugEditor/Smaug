a188b5d3a8acad7043a61cf25b3b26865d0379cc
ozxybox <kieferstashu@gmail.com>
2021-04-11 01:10:29 -0500

    Clear out some old code

--- Smaug/worldeditor.cpp
+++ Smaug/worldeditor.cpp
@@ -1,5 +1,7 @@
 #include "worldeditor.h"
 #include "raytest.h"
+#include "tessellate.h"
+#include "slice.h"
 
 #include <glm/geometric.hpp>
 #include <glm/common.hpp>
@@ -53,6 +55,7 @@ void CNode::Init()
 	m_nodeHeight = 10;
 	ConstructWalls();
 	*/
+
 	for(auto p : m_mesh.parts)
 		defineMeshPartFaces(*p);
 	CalculateAABB();
@@ -66,38 +69,6 @@ void CNode::PreviewUpdate()
 
 void CNode::Update()
 {
-#if 0
-	// Make sure our new position fits our constraints
-	for (int i = 0; i < m_constrainedToCount; i++)
-	{
-		m_constrainedTo[i].Update();
-	}
-#endif
-
-	
-
-#if 0
-	// Update our walls
-	ConstructWalls();
-
-	m_renderData.UpdateBufs();
-
-	// Now that we've updated ourself, we have to update our constraints.
-	for (int i = 0; i < m_constraining.size(); i++)
-	{
-		m_constraining[i]->Update();
-		m_constraining[i]->m_childNode->Update();
-	}
-
-	CalculateAABB();
-
-	// Since we've updated, we have to notify our parents's walls
-	for (int i = 0; i < m_constrainedToCount; i++)
-	{
-		m_constrainedTo[i].m_parentNode->ConstructWalls();
-		m_constrainedTo[i].m_parentNode->m_renderData.Update3DBufs();
-	}
-#endif
 	
 	UpdateThisOnly();
 
@@ -130,179 +101,6 @@ void CNode::UpdateThisOnly()
 }
 
 
-#if 0
-static float FindMaxConstraintHeight(CNode* otherNode, nodeSide_t* otherSide, CNode* ourNode, nodeSide_t* ourSide)
-{
-	// Highest point on the other side's wall + how high the node is + how high the ceiling is. Absolute Max Y for the other side.
-	float constraintMaxY = glm::max(otherSide->vertex1->origin.y, otherSide->vertex2->origin.y) + otherNode->m_origin.y + otherNode->m_nodeHeight;
-
-	// Now that we have how tall the other side is, we have to make it relative to the lowest point on our side
-	constraintMaxY -= glm::max(ourSide->vertex1->origin.y, ourSide->vertex2->origin.y) + ourNode->m_origin.y;
-
-	return constraintMaxY;
-}
-
-// COMPARED ITEMS MUST HAVE THE SAME PARENT!
-int SortSideSideConstraints(const void* a, const void* b)
-{
-	const CConstraint* arg1 = *(const CConstraint**)a;
-	const CConstraint* arg2 = *(const CConstraint**)b;
-	
-	// This is probably slow...
-
-	// If we're not comparing with the same parents, what are we even doing...
-	glm::vec3 parentVert = arg1->m_parentSide->vertex1->origin + arg1->m_parentNode->m_origin;
-
-	// Check our distance from the left side of our parent
-	float arg1Dist = glm::length(parentVert - (arg1->m_childSide->vertex2->origin + arg1->m_childNode->m_origin));
-	float arg2Dist = glm::length(parentVert - (arg2->m_childSide->vertex2->origin + arg2->m_childNode->m_origin));
-
-	if (arg1Dist < arg2Dist) return -1;
-	if (arg1Dist > arg2Dist) return 1;
-	return 0;
-}
-void CNode::ConstructWalls()
-{
-	// We start with discarding our walls and then creating new ones for each side. We then have to cut out of the walls where side->side constraints exist
-
-	// Figure out how tall our node needs to be based on our connected nodes
-	// Sometimes the user might have a height in mind already.
-	if (m_useCalculatedNodeHeight)
-	{
-		float maxY = FLT_MIN;
-
-		// Check what we're constrained to
-		for (int i = 0; i < m_constrainedToCount; i++)
-		{
-			CConstraint* constraint = &m_constrainedTo[i];
-
-			// If it's not a side->side constraint, continue
-			if (constraint->m_childType != ConstraintPairType::SIDE || constraint->m_parentType != ConstraintPairType::SIDE)
-				continue;
-
-			float constraintMaxY = FindMaxConstraintHeight(constraint->m_childNode, constraint->m_childSide, constraint->m_parentNode, constraint->m_parentSide);
-
-			if (constraintMaxY > maxY)
-				maxY = constraintMaxY;
-		}
-
-		// Check what we're constraining
-		for (int i = 0; i < m_constraining.size(); i++)
-		{
-			CConstraint* constraint = m_constraining[i];
-
-			// If it's not a side->side constraint, continue
-			if (constraint->m_childType != ConstraintPairType::SIDE || constraint->m_parentType != ConstraintPairType::SIDE)
-				continue;
-
-			float constraintMaxY = FindMaxConstraintHeight(constraint->m_parentNode, constraint->m_parentSide, constraint->m_childNode, constraint->m_childSide);
-
-			if (constraintMaxY > maxY)
-				maxY = constraintMaxY;
-		}
-
-		// Only change the height if we calculated a new one.
-		if (maxY > FLT_MIN)
-		{
-			m_nodeHeight = maxY;
-		}
-
-	}
-
-
-
-	// Create the walls
-	for (int i = 0; i < m_sideCount; i++)
-	{
-		// Out with the old
-		m_sides[i].walls.clear();
-
-		nodeWall_t wall;
-
-		wall.bottomPoints[0] = m_sides[i].vertex1->origin;
-		wall.bottomPoints[1] = m_sides[i].vertex2->origin;
-
-		wall.topPoints[0] = wall.bottomPoints[0];
-		wall.topPoints[1] = wall.bottomPoints[1];
-
-		// Push the top points up
-		wall.topPoints[0].y += m_nodeHeight;
-		wall.topPoints[1].y += m_nodeHeight;
-
-		// In with the new
-		m_sides[i].walls.push_back(wall);
-	}
-
-
-	// If we're constrained to something, we need to delete its walls
-	for (int i = 0; i < m_constrainedToCount; i++)
-	{
-		CConstraint* cst = &m_constrainedTo[i];
-
-		// If it's a side->side constraint, delete the walls
-		if (cst->m_childType == ConstraintPairType::SIDE && cst->m_parentType == ConstraintPairType::SIDE)
-		{
-			cst->m_childSide->walls.clear();
-		}
-	}
-
-
-	// Figure out what we're constraining and organize them from left to right
-	std::vector<CConstraint*> constraints;
-
-	// Check what we're constraining
-	for (int i = 0; i < m_constraining.size(); i++)
-	{
-		CConstraint* cst = m_constraining[i];
-
-		// If it's a side->side constraint, store it
-		if (cst->m_childType == ConstraintPairType::SIDE && cst->m_parentType == ConstraintPairType::SIDE)
-			constraints.push_back(cst);
-	}
-
-	// Sort the vector from left to right
-	std::qsort(constraints.data(), constraints.size(), sizeof(CConstraint*), SortSideSideConstraints);
-
-
-	// Loop our constraints and cut holes in our walls
-	for (int i = 0; i < constraints.size(); i++)
-	{
-		CConstraint* cst = constraints[i];
-
-		nodeSide_t* parentSide = cst->m_parentSide;
-		nodeSide_t* childSide = cst->m_childSide;
-
-		// If we somehow have no side due to having a parent, bail
-		if (parentSide->walls.size() == 0)
-			continue;
-
-		// Get our right most wall
-		nodeWall_t* lastWall = &parentSide->walls.back();
-		
-		nodeWall_t newWall;
-
-		// Use our last's rightmost's right points for ours
-		newWall.bottomPoints[1] = lastWall->bottomPoints[1];
-		newWall.topPoints[1] = lastWall->topPoints[1];
-
-		// Use our constraint's child's rightmost points as our new leftmost
-		newWall.bottomPoints[0] = childSide->vertex1->origin + cst->m_childNode->m_origin - cst->m_parentNode->m_origin;
-		newWall.topPoints[0] = newWall.bottomPoints[0];
-		newWall.topPoints[0].y += m_nodeHeight; // Gotta make sure we have it high enough!
-
-		// Store it
-		parentSide->walls.push_back(newWall);
-
-		// Use our constraint's child's leftmost points as our last's rightmost
-		lastWall->bottomPoints[1] = childSide->vertex2->origin + cst->m_childNode->m_origin - cst->m_parentNode->m_origin;
-		lastWall->topPoints[1] = lastWall->bottomPoints[1];
-		lastWall->topPoints[1].y += m_nodeHeight; // Gotta make sure we have it high enough!
-
-	}
-
-}
-#endif
-
 bool CNode::IsPointInAABB(glm::vec3 point)
 {
 	return testPointInAABB(point, GetAbsAABB(), 1.25f);
@@ -316,20 +114,6 @@ aabb_t CNode::GetAbsAABB()
 	return aabb;
 }
 
-#if 0
-void CNode::LinkSides()
-{
-	// Link up the sides
-	for (int i = 0; i < m_sideCount - 1; i++)
-	{
-		m_sides[i].vertex1 = &m_vertexes[i];
-		m_sides[i].vertex2 = &m_vertexes[i + 1];
-	}
-	m_sides[m_sideCount - 1].vertex1 = &m_vertexes[m_sideCount - 1];
-	m_sides[m_sideCount - 1].vertex2 = &m_vertexes[0];
-
-}
-#endif
 
 void CNode::CalculateAABB()
 {
@@ -342,17 +126,6 @@ void CNode::CalculateAABB()
 
 CQuadNode::CQuadNode() : CNode()
 {
-#if 0
-	m_nodeType = NodeType::QUAD;
-	m_sideCount = 4;
-	m_vertexes = new nodeVertex_t[4];
-
-	// Start at the left side and rotate clockwise
-	m_vertexes[0].origin = glm::vec3(-1,  0, -1); // Bottom Left
-	m_vertexes[1].origin = glm::vec3(-1,  0,  1); // Top Left
-	m_vertexes[2].origin = glm::vec3( 1,  0,  1); // Top Right
-	m_vertexes[3].origin = glm::vec3( 1,  0, -1); // Bottom Right
-#endif
 	glm::vec3 points[] = {
 
 	{-1,-1,-1}, // bottom back left
@@ -388,165 +161,3 @@ CQuadNode::CQuadNode() : CNode()
 	addMeshFace(m_mesh, &top[0], 4);
 	Init();
 }
-
-/*
-CTriNode::CTriNode()
-{
-	m_nodeType = NodeType::TRI;
-
-	m_sideCount = 3;
-	m_vertexes = new nodeVertex_t[3];
-
-	// Start at the left side
-	m_vertexes[0].origin = glm::vec3( 0,  0, -1); // Bottom Left
-	m_vertexes[1].origin = glm::vec3(-1,  0,  1); // Top Left
-	m_vertexes[2].origin = glm::vec3( 1,  0,  1); // Top Right
-
-	Init();
-}
-*/
-#if 0
-void CConstraint::Update()
-{
-	if(m_parentType == ConstraintPairType::NONE || m_childType == ConstraintPairType::NONE)
-		return; // No constraint. Let's dip.
-
-	if(m_parentType == ConstraintPairType::SIDE && m_childType == ConstraintPairType::VERTEX)
-	{
-		SideVertConstrain();
-		return;
-	}
-
-	if (m_parentType == ConstraintPairType::VERTEX && m_childType == ConstraintPairType::VERTEX)
-	{
-		m_childVertex->origin = m_parentVertex->origin + m_parentNode->m_origin - m_childNode->m_origin;
-
-		return;
-	}
-
-
-	if (m_parentType == ConstraintPairType::SIDE && m_childType == ConstraintPairType::SIDE)
-	{
-		// We have to solve the child side's verts to have the same slope and y-intercept as the parent's side while maintaining the same dist between the verts
-
-		// HACK HACK
-		// Temp set our child to be the verts and constrain them to the parent
-		nodeSide_t* side = m_childSide;
-
-		m_childVertex = side->vertex1;
-		SideVertConstrain();
-
-		m_childVertex = side->vertex2;
-		SideVertConstrain();
-
-		m_childSide = side;
-
-	}
-
-
-
-}
-
-void CConstraint::SwapParentAndChild()
-{
-	ConstraintPairType tempParentType = m_parentType;
-	CNode* tempParentNode = m_parentNode;
-	void* tempParent = m_parent;
-	
-	m_parentType = m_childType;
-	m_parentNode = m_childNode;
-	m_parent = m_child;
-
-	m_childType = tempParentType;
-	m_childNode = tempParentNode;
-	m_child = tempParent;
-}
-
-void CConstraint::SideVertConstrain()
-{
-	// I'm really tired while writing this, so this is probably going to need some revamping at some point.
-	// But this is how it works. We can solve the vertex to the line using two ways: solving to the line using the vert's X, or solving using the Z.
-	// This code solves for both, uses the one with least distance to the line as the solution for snapping, and takes that position's Y for its own.
-
-
-	// If we don't add the home node's origin, all of our math will be totally broken since the verts use local positioning
-	glm::vec3 newOrigin = m_childVertex->origin + m_childNode->m_origin;
-
-	glm::vec3 parentVert1 = m_parentSide->vertex1->origin + m_parentNode->m_origin;
-	glm::vec3 parentVert2 = m_parentSide->vertex2->origin + m_parentNode->m_origin;
-
-
-	glm::vec3 line = parentVert2 - parentVert1;
-
-	// Straight line checking
-	if (line.x == 0)
-	{
-		// Line is straight. Take a shortcut
-		newOrigin.x = parentVert1.x;
-	}
-	else if (line.z == 0)
-	{
-		// Line is straight. Take a shortcut
-		newOrigin.z = parentVert1.z;
-	}
-	else
-	{
-		// The line is not straight. Solve for the new position using the slope
-
-		// Solving for Z, keeping X
-		float slope = line.z / line.x;
-		float zDelta = (newOrigin.x - parentVert1.x) * slope + parentVert1.z - newOrigin.z;
-
-		// Solving for X, keeping Z
-		slope = line.x / line.z;
-		float xDelta = (newOrigin.z - parentVert1.z) * slope + parentVert1.x - newOrigin.x;
-
-		if (abs(xDelta) < abs(zDelta))
-		{
-			// Shortest path to snap is X
-			newOrigin.x += xDelta;
-			printf("solved for X\n");
-		}
-		else
-		{
-			// Shortest path to snap is Z
-			newOrigin.z += zDelta;
-			printf("solved for Z\n");
-		}
-
-		// Find the y for our new X Z
-		//slope = line.y / line.x;
-		//newOrigin.y = (newOrigin.x - parentVert1.x) * slope + parentVert1.y;
-
-	}
-
-	// Lock the vertex to the side
-	// This is probably terrible. Revisit when not tired.
-
-	glm::vec3 midpoint = (parentVert1 + parentVert2) / 2.0f;
-	float linePercent = glm::length(newOrigin - midpoint) / glm::length(parentVert2 - midpoint);
-
-	if (linePercent > 1.0f)
-	{
-		// Out of line. Find nearest and lock to it.
-		// Not how I'd want to do this...
-
-		float lenTo1 = glm::length(parentVert1 - newOrigin);
-		float lenTo2 = glm::length(parentVert2 - newOrigin);
-
-		if (lenTo1 < lenTo2)
-		{
-			newOrigin = parentVert1;
-		}
-		else
-		{
-			newOrigin = parentVert2;
-		}
-	}
-
-	printf("origin - (%f, %f)\tnew - (%f, %f)\n", m_childVertex->origin.x, m_childVertex->origin.z, newOrigin.x, newOrigin.z);
-
-	// Since our origin is local the the home origin, we have to remove it 
-	m_childVertex->origin = newOrigin - m_childNode->m_origin;
-}
-#endif
--- Smaug/worldeditor.h
+++ Smaug/worldeditor.h
@@ -2,87 +2,11 @@
 #include "worldrenderer.h"
 #include "mesh.h"
 #include "meshrenderer.h"
+
 #include <glm/vec3.hpp>
 #include <glm/vec2.hpp>
 #include <vector>
 
-#if 0
-struct nodeSide_t;
-class CNode;
-struct nodeVertex_t;
-
-
-enum class ConstraintPairType
-{
-	NONE,
-	VERTEX,
-	SIDE,
-};
-
-class CConstraint
-{
-public:
-	void Update();
-
-	void SetParent(CNode* node, nodeSide_t* side) { m_parentType = ConstraintPairType::SIDE; m_parentNode = node; m_parentSide = side; }
-	void SetParent(CNode* node, nodeVertex_t* vertex) { m_parentType = ConstraintPairType::VERTEX; m_parentNode = node; m_parentVertex = vertex; }
-
-	void SetChild(CNode* node, nodeSide_t* side) { m_childType = ConstraintPairType::SIDE; m_childNode = node; m_childSide = side; }
-	void SetChild(CNode* node, nodeVertex_t* vertex) { m_childType = ConstraintPairType::VERTEX; m_childNode = node; m_childVertex = vertex; }
-	
-	void SwapParentAndChild();
-
-	void SideVertConstrain();
-
-	ConstraintPairType m_parentType;
-	CNode* m_parentNode;
-	union
-	{
-		void* m_parent;
-		nodeSide_t* m_parentSide;
-		nodeVertex_t* m_parentVertex;
-	};
-
-	ConstraintPairType m_childType;
-	CNode* m_childNode;
-	union
-	{
-		void* m_child;
-		nodeSide_t* m_childSide;
-		nodeVertex_t* m_childVertex;
-	};
-
-};
-
-// Do not change this struct without editing CreateVertexLayout in worldrenderer
-struct nodeVertex_t
-{
-	glm::vec3 origin;
-};
-
-struct nodeWall_t
-{
-	glm::vec3 bottomPoints[2];
-	glm::vec3 topPoints[2];
-};
-
-
-struct nodeSide_t
-{
-	nodeVertex_t* vertex1;
-	nodeVertex_t* vertex2; // Links to the next side's point 1
-
-	std::vector<nodeWall_t> walls;
-};
-
-enum class NodeType
-{
-	NONE,
-	QUAD,
-	TRI,
-};
-
-#endif
 
 class CNode
 {
@@ -117,29 +41,6 @@ public:
 	
 	std::vector<CNode*> m_cutting;
 
-#if 0
-	nodeVertex_t* m_vertexes;
-	nodeSide_t* m_sides;
-	int m_sideCount;
-
-	NodeType m_nodeType;
-
-	glm::vec3 m_origin;
-
-	CNodeRenderData m_renderData;
-
-	std::vector<CConstraint*> m_constraining; // We parent these constraints
-	
-	// Temp Hack!!
-	CConstraint m_constrainedTo[16]; // We are constrained by these constraints 
-	int m_constrainedToCount = 0;
-
-	float m_aabbLength;
-
-	bool m_useCalculatedNodeHeight;
-	float m_nodeHeight;
-
-#endif
 protected:
 	aabb_t m_aabb;
 	bool m_visible;
