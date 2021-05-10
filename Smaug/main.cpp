24e3ce77644a3b7c18d6ece26656d8efdda22758
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2021-05-10 12:51:44 -0700

    Add launch options for opengl (-gl -opengl), vulkan (-vk -vulkan), dx9 (-dx9 -d3d9 -directx9) and dx11 (-dx11 -d3d11 -directx11)

--- Smaug/main.cpp
+++ Smaug/main.cpp
@@ -1,6 +1,25 @@
 #include "smaugapp.h"
+#include "utils.h"
+
+#ifdef _WIN32
+static constexpr auto DEFAULT_RENDER_TYPE = bgfx::RendererType::Direct3D11;
+#else
+static constexpr auto DEFAULT_RENDER_TYPE = bgfx::RendererType::OpenGL;
+#endif 
 
 int main( int argc, char** argv )
 {
-	return GetApp().run( argc, argv );
+	CommandLine::Set(argc, argv);
+	
+	auto renderType = DEFAULT_RENDER_TYPE;
+	if(CommandLine::HasAny("-gl", "-gles"))
+		renderType = bgfx::RendererType::OpenGL;
+	else if(CommandLine::HasAny("-vulkan", "-vk"))
+		renderType = bgfx::RendererType::Vulkan;
+	else if(CommandLine::HasAny("-dx9", "-d3d9", "-directx9"))
+		renderType = bgfx::RendererType::Direct3D9;
+	else if(CommandLine::HasAny("-dx11", "-d3d11", "-directx11"))
+		renderType = bgfx::RendererType::Direct3D11;
+	
+	return GetApp().run( argc, argv, renderType );
 }
